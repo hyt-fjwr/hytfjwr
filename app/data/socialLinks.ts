3fc75e69fa0568ec926236259d2f7c1586026c32
hyt-fjwr <f.hayato0104@gmail.com>
2024-05-29 21:37:55 +0900

    added social links

---
+++ app/data/socialLinks.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+  {
+    name: "Instagram",
+    icon: "/instagram_icon.svg",
+    url: "https://www.instagram.com/_filmoet/",
+  },
+  {
+    name: "YouTube",
+    icon: "/youtube_icon.svg",
+    url: "https://www.youtube.com/channel/UC0iaTS7qQ7V0FVJ9INvr2sA",
+  },
+  {
+    name: "GitHub",
+    icon: "/github_icon.svg",
+    url: "https://github.com/hyt-fjwr",
+  },
+  {
+    name: "Flickr",
+    icon: "/flickr_icon.svg",
+    url: "https://www.flickr.com/photos/186351996@N07/",
+  },
+];
