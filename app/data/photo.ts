fdcea6aff1812346acb77f0f68b2a0a6f9c3e7c8
hyt-fjwr <f.hayato0104@gmail.com>
2024-06-17 13:24:55 +0900

    update photos.

--- app/data/photo.ts
+++ app/data/photo.ts
@@ -14,6 +14,20 @@ export const photos = [
     place: "沖縄 | 美ら海水族館",
   },
   {
+    imageUrl: "/photo/DSC04679.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
+    date: "2024/04/14",
+    place: "沖縄 | 美ら海水族館",
+  },
+  {
+    imageUrl: "/photo/DSC04482.jpg",
+    flickrUrl:
+      "  https://live.staticflickr.com/65535/53686857573_13487c31e9_o.jpg",
+    date: "2024/04/12",
+    place: "沖縄 | シーサイドドライブイン",
+  },
+  {
     imageUrl: "/photo/DSC04340.jpg",
     flickrUrl:
       "https://live.staticflickr.com/65535/53639829355_e1e1f354bc_o.jpg",
@@ -21,18 +35,109 @@ export const photos = [
     place: "山梨 | 小菅村",
   },
   {
-    imageUrl: "/photo/DSC04679.jpg",
+    imageUrl: "/photo/DSC04409.jpg",
     flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  }, ////////////
+      "https://live.staticflickr.com/65535/53639375016_7682e9317d_o.jpg",
+    date: "2024/01/04",
+    place: "東京 | 渋谷区ふれあい植物センター",
+  },
   {
-    imageUrl: "/photo/DSC04679.jpg",
+    imageUrl: "/photo/DSC04411.jpg",
     flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
+      "https://live.staticflickr.com/65535/53639829820_ae3f8f484d_o.jpg",
+    date: "2024/01/04",
+    place: "東京 | 渋谷区ふれあい植物センター",
+  },
+  {
+    imageUrl: "/photo/DSC04257.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/53363287696_17a1eaef3c_o.jpg",
+    date: "2023/11/24",
+    place: "千葉 | 養老渓谷",
+  },
+  {
+    imageUrl: "/photo/DSC04235.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/53363287721_684160ed87_o.jpg",
+    date: "2023/11/24",
+    place: "千葉 | 養老渓谷",
+  },
+  {
+    imageUrl: "/photo/DSC04075.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/53363734425_d828792518_o.jpg",
+    date: "2023/11/24",
+    place: "千葉 | 養老渓谷",
+  },
+  {
+    imageUrl: "/photo/DSC03931.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/53796854165_c67e07afc0_o.jpg",
+    date: "2023/10/14",
+    place: "東京 | 代々木公園",
+  },
+  {
+    imageUrl: "/photo/DSC03034.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52757653755_0a6351e1d1_o.jpg",
+    date: "2023/10/14",
+    place: "福岡 | 博多屋台",
+  },
+  {
+    imageUrl: "/photo/DSC02756.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52705220252_cfb32843e7_o.jpg",
+    date: "2023/02/23",
+    place: "群馬 | 草津温泉",
+  },
+  {
+    imageUrl: "/photo/DSC02605.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52610639740_0a56d7603b_o.jpg",
+    date: "2023/01/01",
+    place: "東京 | 浅草",
+  },
+  {
+    imageUrl: "/photo/DSC02567.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52610638885_6768747f67_o.jpg",
+    date: "2023/01/01",
+    place: "東京 | 浅草",
+  },
+  {
+    imageUrl: "/photo/DSC02499.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52610217001_abb7a22439_o.jpg",
+    date: "2023/01/01",
+    place: "神奈川 | 江の島",
+  },
+  {
+    imageUrl: "/photo/DSC02392.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52610476294_78d4c1ba25_o.jpg",
+    date: "2023/01/01",
+    place: "神奈川 | 江の島",
+  },
+  {
+    imageUrl: "/photo/DSC02114.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52543366771_a08958559a_o.jpg",
+    date: "2022/12/03",
+    place: "東京 | BMW(丸の内)",
+  },
+  {
+    imageUrl: "/photo/DSC02109.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52543368156_a65d2d2e4b_o.jpg",
+    date: "2022/12/03",
+    place: "東京 | BMW(丸の内)",
+  },
+  {
+    imageUrl: "/photo/DSC02000.jpg",
+    flickrUrl:
+      "https://live.staticflickr.com/65535/52490007886_75e3671902_o.jpg",
+    date: "2022/11/06",
+    place: "大阪 | 梅田スカイビル",
   },
   {
     imageUrl: "/photo/DSC01542.jpg",
@@ -42,122 +147,10 @@ export const photos = [
     place: "京都 | 清水寺",
   },
   {
-    imageUrl: "/photo/DSC04679.jpg",
+    imageUrl: "/photo/DSC01408.jpg",
     flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC01542.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/52490465055_76548198e3_o.jpg",
-    date: "2022/11/04",
-    place: "京都 | 清水寺",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC01542.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/52490465055_76548198e3_o.jpg",
-    date: "2022/11/04",
-    place: "京都 | 清水寺",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC01542.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/52490465055_76548198e3_o.jpg",
-    date: "2022/11/04",
-    place: "京都 | 清水寺",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC01542.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/52490465055_76548198e3_o.jpg",
-    date: "2022/11/04",
-    place: "京都 | 清水寺",
-  },
-  {
-    imageUrl: "/photo/DSC04541.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686858598_2efd73dd8e_o.jpg",
-    date: "2024/04/13",
-    place: "沖縄 | 知念岬公園",
-  },
-  {
-    imageUrl: "/photo/DSC04636.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53687002559_df31197954_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC04541.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686858598_2efd73dd8e_o.jpg",
-    date: "2024/04/13",
-    place: "沖縄 | 知念岬公園",
-  },
-  {
-    imageUrl: "/photo/DSC04636.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53687002559_df31197954_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC04541.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686858598_2efd73dd8e_o.jpg",
-    date: "2024/04/13",
-    place: "沖縄 | 知念岬公園",
-  },
-  {
-    imageUrl: "/photo/DSC04636.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53687002559_df31197954_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
-  },
-  {
-    imageUrl: "/photo/DSC04679.jpg",
-    flickrUrl:
-      "https://live.staticflickr.com/65535/53686642146_e2080455e0_o.jpg",
-    date: "2024/04/14",
-    place: "沖縄 | 美ら海水族館",
+      "https://live.staticflickr.com/65535/52490457925_03b5855b3f_o.jpg",
+    date: "2022/10/24",
+    place: "東京 | RX-8(豊洲)",
   },
 ];
