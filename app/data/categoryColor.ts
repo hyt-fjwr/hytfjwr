f6f875d6599a745f57737ac1939f832eec39dd16
hyt-fjwr <f.hayato0104@gmail.com>
2024-08-15 17:12:04 +0900

    Fix problem with hover effect not working on tags. Update packages.

--- app/data/categoryColor.ts
+++ app/data/categoryColor.ts
@@ -2,27 +2,27 @@ export const CategoryColor = [
   {
     Name: "React",
     Color:
-      "text-white dark:border-sky-600 bg-sky-400 dark:bg-sky-900 hover:bg-sky-600 dark:text-sky-300",
+      "text-white dark:border-sky-600 bg-sky-400 dark:bg-sky-900 dark:text-sky-300 hover:scale-105",
   },
   {
     Name: "Nextjs",
     Color:
-      "dark:border-zinc-600 bg-zinc-300 dark:bg-zinc-900 hover:bg-zinc-600 dark:text-zinc-300",
+      "dark:border-zinc-600 bg-zinc-300 dark:bg-zinc-900 dark:text-zinc-300 hover:scale-105",
   },
   {
     Name: "Photograph",
     Color:
-      "text-white dark:border-orange-600 bg-orange-300 dark:bg-orange-900 hover:bg-orange-600 dark:text-orange-300",
+      "text-white dark:border-orange-600 bg-orange-300 dark:bg-orange-900 dark:text-orange-300 hover:scale-105",
   },
   {
     Name: "TEST",
     Color:
-      "text-white dark:border-green-600 bg-green-300 dark:bg-green-900 hover:bg-green-600 dark:text-green-300",
+      "text-white dark:border-green-600 bg-green-300 dark:bg-green-900 dark:text-green-300 hover:scale-105",
   },
   {
     Name: "TypeScript",
     Color:
-      "text-white dark:border-cyan-600 bg-cyan-300 dark:bg-cyan-900 hover:bg-cyan-600 dark:text-cyan-300",
+      "text-white dark:border-cyan-600 bg-cyan-300 dark:bg-cyan-900 dark:text-cyan-300 hover:scale-105",
   },
 ];
 
